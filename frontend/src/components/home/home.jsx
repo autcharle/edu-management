67fbc41cca60ab148f1d354a999bea4d2f75635e
Phan Tấn Kiệt <pthien0135@gmail.com>
2023-05-06 00:50:56 +0700

    login api and raw login UI

--- frontend/src/components/home/home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1>Home page</h1>
-  </React.StrictMode>,
-)
